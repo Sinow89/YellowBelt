0b192c6a69c7093b8171ee483fa0882d4fce207f
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-11-08 12:19:08 +0100

    YellowBelt

---
+++ MiniKata5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MiniKata5;
+
+class Program
+{
+
+    public void Attack(int damage)
+    {
+        Console.WriteLine(damage);
+    }
+    
+    
+    static void Main()
+    {
+        Program program = new Program();
+        program.Attack(10);
+    }
+}
