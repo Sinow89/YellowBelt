d8fc6ef507825d1ae018299960f7ac88b18f90ab
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-11-12 13:59:39 +0100

    Kata6

---
+++ Kata6/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Kata6;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string[] enemiesArray = {"Goblin", "Orc", "Troll", "Skeleton", "Dragon"};
+        var playerInventory = new List<string> { "Sword" , "Shield" , "Potion" };
+        
+        
+
+        Console.WriteLine("Enemies:");
+        foreach (var listEnemiesArray in enemiesArray)
+        {
+            Console.WriteLine(listEnemiesArray);
+        }
+        
+        Console.WriteLine("\nPlayer Inventory:");
+        foreach (var listPlayerInventory in playerInventory)
+        {
+            Console.WriteLine(listPlayerInventory);
+        }
+        
+        playerInventory.Add("Helmet");
+        playerInventory.Add("Armor");
+        playerInventory.Remove("Potion");
+        
+        Console.WriteLine("\nUpdated Inventory:");
+        foreach (var listPlayerInventory in playerInventory)
+        {
+            Console.WriteLine(listPlayerInventory);
+        }
+        
+        // var playerInventroyAmount = playerInventory.Count;
+        
+        Console.WriteLine($"Total Items in Inventory: {playerInventory.Count}");
+        
+    }
+}
