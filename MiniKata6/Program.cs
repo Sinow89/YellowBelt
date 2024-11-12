a24e5913f5e3aea81cfe36a9e3ab779455c7554d
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-11-12 13:22:22 +0100

    Update Program.cs

--- MiniKata6/Program.cs
+++ MiniKata6/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main()
     {
+        string[] enemies = {"Goblin", "Orc", "Troll"};
+        var playerInventory = new List<string>();
+        playerInventory.Add("Sword");
+        playerInventory.Add("Shield");
+        playerInventory.Add("Potion");
+        
+        Console.WriteLine("Enemies");
+        foreach (var listEnemies in enemies) 
+        {
+            Console.WriteLine(listEnemies);
+        }
+Console.WriteLine();
+Console.WriteLine("Player Inventory");
+        foreach (var listPlayerInventory in playerInventory)
+        {
+            Console.WriteLine(listPlayerInventory);
+        }
+
+        playerInventory.Add("Helmet");
+        Console.WriteLine();
+        Console.WriteLine("Updated Inventory");
+        
+        foreach (var listPlayerInventory in playerInventory)
+        {
+            Console.WriteLine(listPlayerInventory);
+        }
         
     }
 }
