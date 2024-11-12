57b2acefa975e5ba5e7fa2da39495e956c0d3a88
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-11-12 15:42:25 +0100

    Update Program.cs

--- MiniKata7/Program.cs
+++ MiniKata7/Program.cs
@@ -6,20 +6,37 @@ class Program
     static void Main()
     {
         
-        Player player = new Player();
-        player.Name = "Arin";
-        player.Health = 100;
-        player.Level = 1;
-
-
+        var player = new Player("Arin", 100, 1);
+        
+        var enemy = new Enemy();
+        
+        Console.WriteLine("Player Info:");
+        Console.WriteLine($"Name: {player.Name}");
+        Console.WriteLine($"Health: {player.Health}");
+        Console.WriteLine($"Level: {player.Level}");
+        
+        Console.WriteLine();
+        
+        Console.WriteLine("Enemy Info:");
+        Console.WriteLine($"Type: {enemy.Type}");
+        Console.WriteLine($"Health: {enemy.Health}");
+        Console.WriteLine($"Damage: {enemy.Damage}");
     }
 }
 
 public class Player
 {
-    public string Name;
-    public int Health;
-    public int Level;
+    public string Name{ get; }
+    public int Health { get; }
+    public int Level{ get; }
+
+    public Player(string name, int health, int level)
+    {
+        Name = name;
+        Health = health;
+        Level = level;
+    }
+   
 }
 
 public class Enemy
