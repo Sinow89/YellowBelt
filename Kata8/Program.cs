084cfbd4b60943b71f2ccf5abce24ad825f5d818
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-11-15 11:36:42 +0100

    Kata8

---
+++ Kata8/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Kata8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+
+
+class Player
+{
+    private int Health { get; set; }
+    private int Level { get; set; }
+    private int Experience { get; set; }
+}
