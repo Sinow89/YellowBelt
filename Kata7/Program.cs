15f5e0aae8ddcff923e73a785e6355944355ef29
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-11-26 11:51:22 +0100

    Update Program.cs

--- Kata7/Program.cs
+++ Kata7/Program.cs
@@ -4,57 +4,53 @@ class Program
 {
     static void Main()
     {
-
-        Player player = new Player(name:"Arin", health: 100, level: 10, experience: 50 );
+        Player player = new Player(name:"Arin", health: 100, level: 10, experience: 50, 50);
         Enemy enemy = new Enemy(type:"Orc", health: 50);
         
-        player.Attack(20, enemy.Type);
-        player.GainExperience(50);
-        enemy.TakeDamage(20);
-        
+        player.Attack(enemy, player);
     }
     
     public class Player
     {
-
-        public void Attack(int damage, string type)
+        public void Attack(Enemy enemy, Player player)
         {
-            Health -= damage;
-            Console.WriteLine($"Player {Name} hit the {type} and deals {damage} damage");
+            Console.WriteLine($"Player {Name} hit the {enemy.Type} and deals {Damage} damage");
+            enemy.TakeDamage(Damage, player);
         }
 
         public void GainExperience(int experience)
         {
             Experience += experience;
-            Console.WriteLine($"Player {Name} gains {Experience} experience points.");
+            Console.WriteLine($"Player {Name} gains {experience} experience points.");
         }
         
-        public Player(string name, int health, int level, int experience)
+        public Player(string name, int health, int level, int experience, int damage)
         {
             Name = name;
             Health = health;
             Level = level;
             Experience = experience;
+            Damage = damage;
         }
         
         public string Name { get; set; }
         public int Health { get; set; }
         public int Level { get; set; }
         public int Experience { get; set; }
-        
+        public int Damage { get; set; }
     }
 
     public class Enemy
     {
-
-        public void TakeDamage(int damage)
+        public void TakeDamage(int damage, Player player)
         {
             Health -= damage;
-            Console.WriteLine($"{Type} hit {damage} damage. Health: {Health}");
+            Console.WriteLine($"{Type} Health: {Health}");
             
             if (Health == 0)
             {
-                
+                Console.WriteLine($"{Type} died!");
+                player.GainExperience(50);
             }
         }
 
@@ -66,6 +62,5 @@ class Program
             Type = type;
             Health = health;
         }
-
     }
 }
